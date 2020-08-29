177996550aa0fcb1f93ad0b9e3ef147f9c7eb33a
vmwork <v.marwork@gmail.com>
2020-08-29 20:03:29 +0300

    scrolling

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,25 @@
 (function () {
 
-        // плавный скроллинг ----------------------------
-        // document.querySelectorAll('a[href^="#"').forEach(link => {
 
-        //     link.addEventListener('click', function(e) {
-        //         e.preventDefault();
+        document.querySelectorAll('a[href^="#"').forEach(link => {
+
+            link.addEventListener('click', function(e) {
+                e.preventDefault();
         
-        //         let href = this.getAttribute('href').substring(1);
+                let href = this.getAttribute('href').substring(1);
         
-        //         const scrollTarget = document.getElementById(href);
+                const scrollTarget = document.getElementById(href);
         
-        //         const topOffset = document.querySelector('.scrollto').offsetHeight;
-        //         // const topOffset = 0; // если не нужен отступ сверху 
-        //         const elementPosition = scrollTarget.getBoundingClientRect().top;
-        //         const offsetPosition = elementPosition - topOffset;
+                const topOffset = document.querySelector('.scrollto').offsetHeight;
+                // const topOffset = 0; // если не нужен отступ сверху 
+                const elementPosition = scrollTarget.getBoundingClientRect().top;
+                const offsetPosition = elementPosition - topOffset;
         
-        //         window.scrollBy({
-        //             top: offsetPosition,
-        //             behavior: 'smooth'
-        //         });
-        //     });
-        // });
-// плавный скроллинг END----------------------------
+                window.scrollBy({
+                    top: offsetPosition,
+                    behavior: 'smooth'
+                });
+            });
+        });
        
 })();
